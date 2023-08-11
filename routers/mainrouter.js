2133cb577a6bd2e5c13d38cc940b847762369225
mhubrich <mhubrich.2011@gmail.com>
2023-08-11 13:35:58 -0700

    implement GET /movies

--- routers/mainrouter.js
+++ routers/mainrouter.js
@@ -5,6 +5,14 @@ const RecordList = require("../utils/recordlist");
 const mainRouter = express.Router();
 
 mainRouter.get("/", (req, res, next) => {
+    if (!req.hasOwnProperty('db') || typeof req.db === "undefined") {
+        return res.status(500).send("Could not find database.");
+    }
+    const recordList = req.db.getAll();
+    if (typeof recordList === "undefined" || !(recordList instanceof RecordList)) {
+        return res.status(500).send("Could not retrieve records from database.");
+    }
+    res.status(200).json(RecordList.toJSON(recordList));
     next();
 });
 
