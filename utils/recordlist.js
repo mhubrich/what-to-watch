14b257f6d08bb762c0fe6e4cea99eccc742972e0
mhubrich <mhubrich.2011@gmail.com>
2023-08-15 12:04:49 -0700

    refactor RecordList

--- utils/recordlist.js
+++ utils/recordlist.js
@@ -1,7 +1,3 @@
-const { Movie } = require("./movie");
-const { Record, RecordMeta } = require("./record");
-
-
 class RecordList {
     constructor() {
         this.records = [];
@@ -25,29 +21,6 @@ class RecordList {
 
     length() {
         return this.records.length;
-    }
-
-    // TODO are those two methods needed?
-    static toJSON(recordList) {
-        return JSON.stringify(recordList);
-    }
-
-    static fromJSON(data) {
-        const recordList = new RecordList();
-        for (const rec of JSON.parse(data).records) {
-            const movie = new Movie(rec.movie.id,
-                                    rec.movie.name,
-                                    rec.movie.type,
-                                    rec.movie.poster,
-                                    rec.movie.rating,
-                                    rec.movie.summary);
-            const recordMeta = new RecordMeta(rec.meta.id,
-                                              rec.meta.userId,
-                                              rec.meta.dateAdded);
-            const record = new Record(movie, recordMeta);
-            recordList.add(record);
-        }
-        return recordList;
     }
 }
 
