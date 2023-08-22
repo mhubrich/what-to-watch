61f15c8d2cbe09e55a9eba7357b05e77cdf0ea00
mhubrich <mhubrich.2011@gmail.com>
2023-08-22 15:20:22 -0700

    add documentation and comments

--- routers/authrouter.js
+++ routers/authrouter.js
@@ -1,3 +1,9 @@
+/**
+ * Express Router that configures 
+ * 1) Authentication with Google OAuth 2.0
+ * 2) Login session management
+ * 3) Session storage
+ */
 const express = require("express");
 const session = require('express-session');
 const passport = require("passport");
@@ -6,14 +12,15 @@ const DynamoDBStore = require("dynamodb-store");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 
+// Creates an Express Router which is later exported as a module
 const authRouter = express.Router();
 
-// Send back a site verification code to enable domain authorization (Google OAuth2)
+// Sends back a site verification code to enable domain authorization (Google OAuth2)
 authRouter.get("/", (req, res, next) => {
     res.send(config.get("auth-provider.google.siteVerification"));
 });
 
-// Configure session storage on DynamoDB
+// Configures session storage on DynamoDB
 authRouter.use(session({
     secret: config.get("session.secret"),
     resave: false, 
@@ -55,7 +62,11 @@ passport.deserializeUser((user, done) => {
     done(null, user);
 });
 
-// Helper to obtain a display name from the given profile
+/**
+ * Helper to obtain a display name from the given profile.
+ * @param {Object} profile  Profile obtained by the identity provider.
+ * @return {String}         Display name of the user.
+ */
 function getName(profile) {
     if (profile.hasOwnProperty("name") && profile.name.hasOwnProperty("givenName")) {
         return profile.name.givenName;
@@ -66,7 +77,14 @@ function getName(profile) {
     return "Unknown User";
 }
 
-// Middleware function to verify if user is authenticated
+/**
+ * Middleware function to verify if user is authenticated.
+ * @param {Request} req     Request object.
+ * @param {Response} res    Response object.
+ * @param {Function} next   Callback to invoke the next middleware function.
+ * @return {Object}         Response with status code 401 if user is not authentication,
+ *                          invokes the callback `next` otherwise.
+ */
 function isAuthenticated(req, res, next) {
     if (!req.isAuthenticated()) return res.status(401).send("No Access");
     next();
