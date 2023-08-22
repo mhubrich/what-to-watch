5c479b2bd739e4fe880bc5b8bcf36078429a3392
mhubrich <mhubrich.2011@gmail.com>
2023-08-21 19:53:47 -0700

    add comments

--- routers/authrouter.js
+++ routers/authrouter.js
@@ -8,6 +8,12 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const authRouter = express.Router();
 
+// Send back a site verification code to enable domain authorization (Google OAuth2)
+authRouter.get("/", (req, res, next) => {
+    res.send(config.get("auth-provider.google.siteVerification"));
+});
+
+// Configure session storage on DynamoDB
 authRouter.use(session({
     secret: config.get("session.secret"),
     resave: false, 
@@ -19,14 +25,16 @@ authRouter.use(session({
             secretAccessKey: config.get("database.users.secretAccessKey"),
             region: config.get("database.users.region")
         },
-        ttl: 86400000 // 1 day
+        // Time to live = 1 day (removes any session data older than ttl)
+        ttl: 86400000
     }),
 }));
 
+// Enable session authentication
 authRouter.use(passport.initialize());
 authRouter.use(passport.session());
 
-
+// Configure the Google OAuth 2.0 strategy
 passport.use(new GoogleStrategy({
         clientID: config.get("auth-provider.google.clientId"),
         clientSecret: config.get("auth-provider.google.clientSecret"),
@@ -38,16 +46,19 @@ passport.use(new GoogleStrategy({
     }
 ));
 
+// Serialize user information to the session store
 passport.serializeUser((user, done) => {
     console.log("SERIALIZE USER", user)
     done(null, user);
 });
 
+// Deserialize user information from the session store
 passport.deserializeUser((user, done) => {
     console.log("DESERIALIZE USER", user);
     done(null, user);
 });
 
+// Helper to obtain a display name from the given profile
 function getName(profile) {
     if (profile.hasOwnProperty("name") && profile.name.hasOwnProperty("givenName")) {
         return profile.name.givenName;
@@ -58,15 +69,12 @@ function getName(profile) {
     return "Unknown User";
 }
 
-// Send back a site verification code to enable domain authorization (Google OAuth2)
-authRouter.get("/", (req, res, next) => {
-    res.send(config.get("auth-provider.google.siteVerification"));
-});
-
+// Middleware function to verify if user is authenticated
 function isAuthenticated(req, res, next) {
     console.log("isAuthenticated", req);
     if (!req.isAuthenticated()) return res.status(401).send("No Access");
     next();
 }
+
 
 module.exports = { authRouter, isAuthenticated };
