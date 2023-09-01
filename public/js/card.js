540098ebbb13e1326a7913c9d9e76d6ce9e84faa
mhubrich <mhubrich.2011@gmail.com>
2023-09-01 16:23:11 -0700

    add badges for type of movie and star rating

--- public/js/card.js
+++ public/js/card.js
@@ -30,17 +30,53 @@ class MovieCard extends Card {
     static createCard(record, cb) {
         const cardTitle = Card.createElement("h1", "card-title");
         cardTitle.innerHTML = record.movie.name;
+        const cardBadgeType = Card.createElement("div", "badge");
+        cardBadgeType.innerHTML = record.movie.type.name;
+        const cardBadgeRating = MovieCard.createStarRating(record.movie.rating);
+        const cardBadges = Card.createElement("div", "card-badges", [cardBadgeType, cardBadgeRating]);
         const cardSummary = Card.createElement("p", "card-summary");
         cardSummary.innerHTML = record.movie.summary;
         const cardButton = Card.createElement("button", "card-btn-remove");
         cardButton.innerHTML = "Remove";
         cardButton.addEventListener("click", () => cb(record.meta.id));
-        const cardBody = Card.createElement("div", "card-body", [cardTitle, cardSummary, cardButton]);
+        const cardBody = Card.createElement("div", "card-body", [cardTitle, cardBadges, cardSummary, cardButton]);
         const cardImg = Card.createElement("img", "card-img");
         cardImg.src = record.movie.poster;
         cardImg.alt = record.movie.name;
         const card = Card.createElement("div", "card", [cardImg, cardBody]);
         return card;
+    }
+
+    static toNearestHalf(rating) {
+        return Math.round(rating * 2) / 2;
+    }
+
+    static getStars(rating) {
+        const full = Math.floor(rating);
+        const half = Math.ceil(rating) - rating;
+        const empty = 5 - Math.ceil(half) - full;
+        return {
+            full: full,
+            half: half,
+            empty: empty
+        }
+    }
+
+    static createStarRating(rating) {
+        rating = rating / 2;
+        rating = MovieCard.toNearestHalf(rating);
+        const badges = Card.createElement("div", "badge");
+        const stars = MovieCard.getStars(rating);
+        for (let i = 0; i < stars.full; i++) {
+            badges.appendChild(Card.createElement("span", "fa fa-star star"));
+        }
+        for (let i = 0; i < stars.half; i++) {
+            badges.appendChild(Card.createElement("span", "fa fa-star-half-stroke star"));
+        }
+        for (let i = 0; i < stars.empty; i++) {
+            badges.appendChild(Card.createElement("span", "fa-regular fa-star star"));
+        }
+        return badges;
     }
 }
 
