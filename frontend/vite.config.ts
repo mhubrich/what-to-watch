85cc3689cd85045a6279841db0dbc7f1a75efedd
mhubrich <mhubrich.2011@gmail.com>
2026-03-27 18:42:02 -0300

    fixes local dev setup issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  envDir: '../',
   base: '/what-to-watch/',
   plugins: [react(), tailwindcss()],
   resolve: {
