7a85eae3aa767f37179e2d29b472042b6d42c3d7
mhubrich <mhubrich.2011@gmail.com>
2026-03-27 18:42:26 -0300

    fixes the movie search by changing the user agent

--- routers/router-search.js
+++ routers/router-search.js
@@ -87,7 +87,8 @@ searchRouter.get("/:id", async (req, res, next) => {
     Promise.resolve(url)
     .then(url => fetch(url, {
         headers: {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+            "Accept-Language": "en-US,en;q=0.5"
         }
     }))
     .then(async response => {
